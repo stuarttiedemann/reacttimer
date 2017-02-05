712dc3c26eb8dd1ce85c09d5e644ede9302dcc54
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-02-05 15:57:16 -0600

    added clock styles and tests

--- app/tests/components/clock.test.jsx
+++ app/tests/components/clock.test.jsx
@@ -10,6 +10,15 @@ describe('Clock', () => {
   it('should exist', () => {
     expect(Clock).toExist();
   });
+  describe('render', ()=> {
+    it('should render clock to output', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+      var $el = $(ReactDOM.findDOMNode(clock));
+      var actualText = $el.find('.clock-text').text();
+
+      expect(actualText).toBe('01:02');
+    });
+  });
 
   describe('formatSeconds', () => {
     it('should format seconds', () => {
